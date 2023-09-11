9c7a9129f802d828cb9560c753fb5ca5252cf938
Shahbaz Alam <shahbazal@infinite.com>
2023-09-11 10:44:42 +0530

    C# Test

---
+++ C# Code Based Test/Csharp_Test_1/Csharp_Test_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Test_1
+{
+    class Test_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveCharacter("Python", 1));
+            Console.WriteLine(RemoveCharacter("Python", 0));
+            Console.WriteLine(RemoveCharacter("Python", 4));
+            Console.WriteLine(".....................................");
+
+            Console.Write("Enter the number of which Table you want: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Multiplication(number);
+
+            Console.ReadLine();
+        }
+
+        public static string RemoveCharacter(string input, int position)
+        {
+            Console.WriteLine(".............Solution of Question-1.............. ");
+            if (position < 0 || position >= input.Length)
+            {
+                // If the position is not in range, return the original .
+                return input;
+            }
+
+            string result = input.Remove(position, 1);
+
+            return result;
+        }
+
+
+        public static void Multiplication(int number)
+        {
+            Console.WriteLine(".............Solution of Question-2..............");
+            Console.WriteLine($"Multiplication Table for {number}:");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+            }
+        }
+
+
+    }
+
+}
+

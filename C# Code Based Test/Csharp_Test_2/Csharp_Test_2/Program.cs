ecf3a02b41c85e971cb06bfd5e347bfa55f05bd0
Shahbaz Alam <shahbazal@infinite.com>
2023-09-19 10:50:29 +0530

    Test-2

---
+++ C# Code Based Test/Csharp_Test_2/Csharp_Test_2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Test_2
+{
+    // Abstract class Student
+    public abstract class Student
+    {
+        public string Name;
+        public int StudentId;
+        public double Grade;
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    // Subclass Undergraduate
+    public class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+
+    // Subclass Graduate
+    public class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter details for an Undergraduate student:");
+            Undergraduate undergraduateStudent = UndergraduateDetails();
+
+            Console.WriteLine("Enter details for a Graduate student:");
+            Graduate graduateStudent = GraduateDetails();
+
+            // Checking Undergraduate student result
+            Console.WriteLine("Result for Undergraduate ");
+            Console.WriteLine("Name: " + undergraduateStudent.Name);
+            Console.WriteLine("Student ID: " + undergraduateStudent.StudentId);
+            bool UndergraduatePassed = undergraduateStudent.IsPassed(undergraduateStudent.Grade);
+            Console.WriteLine("Passed: " + UndergraduatePassed);
+
+
+            // Checking  Graduate student result
+           Console.WriteLine("Result for Graduate ");
+            Console.WriteLine("Name: " + graduateStudent.Name);
+            Console.WriteLine("Student ID: " + graduateStudent.StudentId);
+            bool GraduatePassed = graduateStudent.IsPassed(graduateStudent.Grade);
+            Console.WriteLine("Passed: " + GraduatePassed);
+
+            Console.ReadLine();
+        }
+
+        public static Undergraduate UndergraduateDetails()
+        {
+            Undergraduate student = new Undergraduate();
+
+            Console.Write("Enter Name: ");
+            student.Name = Console.ReadLine();
+
+            Console.Write("Enter Student ID: ");
+            student.StudentId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Grade: ");
+            student.Grade = Convert.ToDouble(Console.ReadLine());
+
+            return student;
+        }
+
+        static Graduate GraduateDetails()
+        {
+            Graduate student = new Graduate();
+
+            Console.Write("Enter Name: ");
+            student.Name = Console.ReadLine();
+
+            Console.Write("Enter Student ID: ");
+            student.StudentId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Grade: ");
+            student.Grade = Convert.ToDouble(Console.ReadLine());
+
+            return student;
+        }
+    }
+
+
+
+}
---
+++ C# Code Based Test/Csharp_Test_2/Csharp_Test_2/Solution_2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Test_2
+{
+    class Product
+    {
+        public int ProductId;
+        public string ProductName;
+        public double Price;
+    }
+
+    class Solution_2
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            // here entry of 10 products
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i}:");
+                Product product = new Product();
+
+                Console.Write("Product ID: ");
+                product.ProductId = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Product Name: ");
+                product.ProductName = Console.ReadLine();
+
+                Console.Write("Price: ");
+                product.Price = Convert.ToDouble(Console.ReadLine());
+
+                products.Add(product);
+
+                
+            }
+
+            // Sorting  products based on price
+            List<Product> sortedProducts = products.OrderBy(p => p.Price).ToList();
+
+            // Displaying the sorted products
+            Console.WriteLine("\nSorted Products by Price:");
+            foreach (var product in sortedProducts)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Price: {product.Price}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}

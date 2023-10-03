e96a594ec1e146c5b5b0ac06d4e3e8e7e32d15ca
Md Shahbaz Alam <shahbazal@infinite.com>
2023-10-03 18:21:20 +0530

    C sharp test 4

--- C# Code Based Test/CSharp_Test_4/CSharp_Test_4/Solution_3.cs
+++ C# Code Based Test/CSharp_Test_4/CSharp_Test_4/Solution_3.cs
@@ -24,16 +24,16 @@ namespace CSharp_Test_4
             
             List<Employee> employeeList = new List<Employee>
             {
-            new Employee { EmployeeID = 1001, FirstName = "Malcolm", LastName = "Daruwalla", Title = "Manager", DOB = DateTime.Parse("16/11/1984"), DOJ = DateTime.Parse("8/6/2011"), City = "Mumbai" },
-            new Employee { EmployeeID = 1002, FirstName = "Asdin", LastName = "Dhalla", Title = "AsstManager", DOB = DateTime.Parse("20/08/1984"), DOJ = DateTime.Parse("7/7/2012"), City = "Mumbai" },
-            new Employee { EmployeeID = 1003, FirstName = "Madhavi", LastName = "Oza", Title = "Consultant", DOB = DateTime.Parse("14/11/1987"), DOJ = DateTime.Parse("12/4/2015"), City = "Pune" },
-            new Employee { EmployeeID = 1004, FirstName = "Saba", LastName = "Shaikh", Title = "SE", DOB = DateTime.Parse("3/6/1990"), DOJ = DateTime.Parse("2/2/2016"), City = "Pune" },
-            new Employee { EmployeeID = 1005, FirstName = "Nazia", LastName = "Shaikh", Title = "SE", DOB = DateTime.Parse("8/3/1991"), DOJ = DateTime.Parse("2/2/2016"), City = "Mumbai" },
-            new Employee { EmployeeID = 1006, FirstName = "Amit", LastName = "Pathak", Title = "Consultant", DOB = DateTime.Parse("7/11/1989"), DOJ = DateTime.Parse("8/8/2014"), City = "Chennai" },
-            new Employee { EmployeeID = 1007, FirstName = "Vijay", LastName = "Natrajan", Title = "Consultant", DOB = DateTime.Parse("2/12/1989"), DOJ = DateTime.Parse("1/6/2015"), City = "Mumbai" },
-            new Employee { EmployeeID = 1008, FirstName = "Rahul", LastName = "Dubey", Title = "Associate", DOB = DateTime.Parse("11/11/1993"), DOJ = DateTime.Parse("6/11/2014"), City = "Chennai" },
-            new Employee { EmployeeID = 1009, FirstName = "Suresh", LastName = "Mistry", Title = "Associate", DOB = DateTime.Parse("12/8/1992"), DOJ = DateTime.Parse("3/12/2014"), City = "Chennai" },
-            new Employee { EmployeeID = 1010, FirstName = "Sumit", LastName = "Shah", Title = "Manager", DOB = DateTime.Parse("12/4/1991"), DOJ = DateTime.Parse("2/1/2016"), City = "Pune" }
+            new Employee { EmployeeID = 1001, FirstName = "Malcolm", LastName = "Daruwalla", Title = "Manager", DOB = Convert.ToDateTime("16/11/1984"), DOJ =  Convert.ToDateTime("8/6/2011"), City = "Mumbai" },
+            new Employee { EmployeeID = 1002, FirstName = "Asdin", LastName = "Dhalla", Title = "AsstManager", DOB =  Convert.ToDateTime("20/08/1984"), DOJ =  Convert.ToDateTime("7/7/2012"), City = "Mumbai" },
+            new Employee { EmployeeID = 1003, FirstName = "Madhavi", LastName = "Oza", Title = "Consultant", DOB =  Convert.ToDateTime("14/11/1987"), DOJ =  Convert.ToDateTime("12/4/2015"), City = "Pune" },
+            new Employee { EmployeeID = 1004, FirstName = "Saba", LastName = "Shaikh", Title = "SE", DOB =  Convert.ToDateTime("3/6/1990"), DOJ =  Convert.ToDateTime("2/2/2016"), City = "Pune" },
+            new Employee { EmployeeID = 1005, FirstName = "Nazia", LastName = "Shaikh", Title = "SE", DOB =  Convert.ToDateTime("8/3/1991"), DOJ =  Convert.ToDateTime("2/2/2016"), City = "Mumbai" },
+            new Employee { EmployeeID = 1006, FirstName = "Amit", LastName = "Pathak", Title = "Consultant", DOB =  Convert.ToDateTime("7/11/1989"), DOJ =  Convert.ToDateTime("8/8/2014"), City = "Chennai" },
+            new Employee { EmployeeID = 1007, FirstName = "Vijay", LastName = "Natrajan", Title = "Consultant", DOB =  Convert.ToDateTime("2/12/1989"), DOJ =  Convert.ToDateTime("1/6/2015"), City = "Mumbai" },
+            new Employee { EmployeeID = 1008, FirstName = "Rahul", LastName = "Dubey", Title = "Associate", DOB =  Convert.ToDateTime("11/11/1993"), DOJ =  Convert.ToDateTime("6/11/2014"), City = "Chennai" },
+            new Employee { EmployeeID = 1009, FirstName = "Suresh", LastName = "Mistry", Title = "Associate", DOB =  Convert.ToDateTime("12/8/1992"), DOJ =  Convert.ToDateTime("3/12/2014"), City = "Chennai" },
+            new Employee { EmployeeID = 1010, FirstName = "Sumit", LastName = "Shah", Title = "Manager", DOB =  Convert.ToDateTime("12/4/1991"), DOJ =  Convert.ToDateTime("2/1/2016"), City = "Pune" }
              };
 
             Console.WriteLine("A. Details of all employees:\n");

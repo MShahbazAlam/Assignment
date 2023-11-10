586971b0efe864d01f3e726720e895303909f4b1
Md Shahbaz Alam <shahbazal@infinite.com>
2023-11-10 17:55:12 +0530

    MVC Test

---
+++ Code Based Test/MVC_Code_Based_8/Solution_2/Models/MoviesDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Solution_2.Models
+{
+    public class MoviesDB : DbContext
+    {
+        //    public DbSet<Movies> Movie { get; set; }
+
+        public DbSet<Movies> Movie { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movies>().HasKey(m => m.Mid);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

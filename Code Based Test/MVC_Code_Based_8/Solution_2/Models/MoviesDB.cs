b8758ff36f78cc8a2311d83bcdc256285e7bba7b
Md Shahbaz Alam <shahbazal@infinite.com>
2023-11-14 11:05:24 +0530

    MVC CODE TEST 8

--- Code Based Test/MVC_Code_Based_8/Solution_2/Models/MoviesDB.cs
+++ Code Based Test/MVC_Code_Based_8/Solution_2/Models/MoviesDB.cs
@@ -8,19 +8,13 @@ using System.Data.Entity;
 namespace Solution_2.Models
 {
     public class MoviesDB : DbContext
-    {
-<<<<<<< HEAD
+    { 
         public MoviesDB() :base("MoviesDB")
-=======
-         public MoviesDB() :base("MoviesDB")
->>>>>>> f7656b70eaccbaa526fee63d0be314388f2da0ac
+
         {
 
         }
         public DbSet<Movies> Movie { get; set; }
-<<<<<<< HEAD
-        
-=======
->>>>>>> f7656b70eaccbaa526fee63d0be314388f2da0ac
+
     }
 }
